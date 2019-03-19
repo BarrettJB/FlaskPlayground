0d1c83063ce426c63708edef561338675951484f
Barrett Bryson <bbryson1@lenovo.com>
2019-03-19 14:31:47 -0400

    Made playground to start learning more about html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+@app.route('/test')
+def test():
+    return render_template('playground.html')
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)

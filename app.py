9f587c2347bef3843c2798339de7b6a8e22c3761
Barrett Bryson <bbryson1@lenovo.com>
2019-03-20 14:28:34 -0400

    Starting to learn some stuff about html and css

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def test():
     return render_template('playground.html')
 
+@app.route('/html_reference')
+def html_reference():
+    return render_template('reference.html')
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)
